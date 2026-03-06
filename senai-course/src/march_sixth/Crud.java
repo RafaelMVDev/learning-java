20e98c47604369f6d813f3da3176f8687592fc80
RafaelMVDev <rafaelmoralesvillela@gmail.com>
2026-03-06 20:49:50 -0300

    feat Crud class

---
+++ senai-course/src/march_sixth/Crud.java
@@ -0,0 +1,15 @@
+package march_sixth;
+
+public abstract class Crud {
+    // Métodos obrigatórios
+    public abstract void cadastrar();
+    public abstract void selecionar();
+    public abstract void alterar();
+    public abstract void remover();
+
+    // Métodos opcionais
+
+    public void mensagem(){
+        System.out.println("Método opcional...");
+    }
+}
