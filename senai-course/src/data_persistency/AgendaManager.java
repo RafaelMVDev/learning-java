bebcf773f81f924e0542c9c0f128c3e968685620
RafaelMVDev <rafaelmoralesvillela@gmail.com>
2026-03-19 21:05:23 -0300

    feat: Agenda Manager class

--- senai-course/src/data_persistency/AgendaManager.java
+++ senai-course/src/data_persistency/AgendaManager.java
@@ -1,9 +1,31 @@
-package data_persistency;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
 public class AgendaManager {
     private final String nomeArquivo;
 
-    public AgendaManager(String nomeArquivo)
+    public AgendaManager(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public void salvarContatos(List<Contato> contatos) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nomeArquivo))) {
+            oos.writeObject(contatos);
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar contatos: " + e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Contato> carregarContatos() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomeArquivo))) {
+            return (List<Contato>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            return new ArrayList<>(); // Retorna uma lista vazia se o arquivo ainda não existir
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Erro ao carregar contatos: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
 }
